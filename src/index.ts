707f713c1fee1af29e462d04cd1515c917bd786d
Asadullah Rifat <asadullah.rifat>
2023-04-19 02:46:15 +0600

    loader scene developed

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { Manager } from './Manager';
-import { GameScene } from './GameScene';
 import { Constants } from './Constants';
+import { LoaderScene } from './LoaderScene';
 
 Manager.initialize(Constants.DEFAULT_GAME_VIEW_WIDTH, Constants.DEFAULT_GAME_VIEW_HEIGHT, 0x464646);
 
 // Set the game scene
-const gameScene: GameScene = new GameScene();
-Manager.changeScene(gameScene);
+const loaderScene: LoaderScene = new LoaderScene();
+Manager.changeScene(loaderScene);
 
 // Resize the scene
 Manager.resize();
