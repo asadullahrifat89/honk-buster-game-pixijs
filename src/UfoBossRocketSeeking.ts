523e0a057678f9ea6530099238e07547fada4f2d
Asadullah Rifat <asadullah.rifat>
2023-04-24 18:59:06 +0600

    mafia boss rocket bulls eye developed

--- src/UfoBossRocketSeeking.ts
+++ src/UfoBossRocketSeeking.ts
@@ -5,43 +5,44 @@ import { SeekingRocketBase } from './SeekingRocketBase';
 
 export class UfoBossRocketSeeking extends SeekingRocketBase {
 
-    private _autoBlastDelay: number = 0;
-    private readonly _autoBlastDelayDefault: number = 25;
+	private _autoBlastDelay: number = 0;
+	private readonly _autoBlastDelayDefault: number = 25;
 
-    constructor(speed: number) {
-        super(speed);
-    }
+	constructor(speed: number) {
+		super(speed);
+	}
 
-    reset() {
-        this.alpha = 1;
-        this.setTexture(Constants.getRandomTexture(ConstructType.UFO_BOSS_ROCKET_SEEKING));
-        this.scale.set(1);
-        this.angle = 0;
-        this.isBlasting = false;
-        this._autoBlastDelay = this._autoBlastDelayDefault;
-    }
+	reset() {
+		this.alpha = 1;
+		this.setTexture(Constants.getRandomTexture(ConstructType.UFO_BOSS_ROCKET_SEEKING));
+		this.scale.set(1);
+		this.angle = 0;
+		this.isBlasting = false;
+		this._autoBlastDelay = this._autoBlastDelayDefault;
+	}
 
-    reposition(source: GameObject) {
-        this.setPosition(source.getLeft() + 15 - this.width / 2, source.getTop() + this.height + 15);
-    }
+	reposition(source: GameObject) {
+		this.setPosition(source.getLeft() + 15 - this.width / 2, source.getTop() + this.height + 15);
+	}
 
-    setBlast() {
-        this.speed = Constants.DEFAULT_CONSTRUCT_SPEED - 1;
+	setBlast() {
+		this.speed = Constants.DEFAULT_CONSTRUCT_SPEED - 1;
 
-        this.scale.set(Constants.DEFAULT_BLAST_SHRINK_SCALE);
-        this.angle = 0;
+		this.scale.set(Constants.DEFAULT_BLAST_SHRINK_SCALE);
+		this.angle = 0;
 
-        this.setTexture(Constants.getRandomTexture(ConstructType.BLAST));
+		this.setTexture(Constants.getRandomTexture(ConstructType.BLAST));
 
-        this.isBlasting = true;
-    }
+		this.isBlasting = true;
+	}
 
-    autoBlast() {
-        this._autoBlastDelay -= 0.1;
+	autoBlast() {
+		this._autoBlastDelay -= 0.1;
 
-        if (this._autoBlastDelay <= 0)
-            return true;
+		if (this._autoBlastDelay <= 0)
+			return true;
 
-        return false;
-    }
+		return false;
+	}
 }
+
