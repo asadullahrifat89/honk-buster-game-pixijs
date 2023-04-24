cdbed9bd8aab8b92d7e993c8cc84c439ffcb1fa2
Asadullah Rifat <asadullah.rifat>
2023-04-24 13:15:02 +0600

    ufo boss rocket seeking developed

--- src/SeekingRocketBase.ts
+++ src/SeekingRocketBase.ts
@@ -4,52 +4,53 @@ import { GameObject } from './GameObject';
 
 export class SeekingRocketBase extends GameObject {
 
-    private readonly _grace: number = 7;
-    private readonly _lag: number = 60;
+	private readonly _grace: number = 7;
+	private readonly _lag: number = 60;
 
-    seek(target: Rectangle, doubleSpeed: boolean) {
-        let left = this.getLeft();
-        let top = this.getTop();
+	seek(target: Rectangle, doubleSpeed: boolean) {
+		let left = this.getLeft();
+		let top = this.getTop();
 
-        let playerMiddleX = left + this.width / 2;
-        let playerMiddleY = top + this.height / 2;
+		let playerMiddleX = left + this.width / 2;
+		let playerMiddleY = top + this.height / 2;
 
-        // move up
-        if (target.y < playerMiddleY - this._grace) {
-            var distance = Math.abs(target.y - playerMiddleY);
-            let speed = this.calculateSpeed(distance, doubleSpeed);
+		// move up
+		if (target.y < playerMiddleY - this._grace) {
+			var distance = Math.abs(target.y - playerMiddleY);
+			let speed = this.calculateSpeed(distance, doubleSpeed);
 
-            this.y = (top - speed);
-        }
+			this.y = (top - speed);
+		}
 
-        // move left
-        if (target.x < playerMiddleX - this._grace) {
-            var distance = Math.abs(target.x - playerMiddleX);
-            let speed = this.calculateSpeed(distance, doubleSpeed);
+		// move left
+		if (target.x < playerMiddleX - this._grace) {
+			var distance = Math.abs(target.x - playerMiddleX);
+			let speed = this.calculateSpeed(distance, doubleSpeed);
 
-            this.x = (left - speed);
-        }
+			this.x = (left - speed);
+		}
 
-        // move down
-        if (target.y > playerMiddleY + this._grace) {
-            var distance = Math.abs(target.y - playerMiddleY);
-            let speed = this.calculateSpeed(distance, doubleSpeed);
+		// move down
+		if (target.y > playerMiddleY + this._grace) {
+			var distance = Math.abs(target.y - playerMiddleY);
+			let speed = this.calculateSpeed(distance, doubleSpeed);
 
-            this.y = (top + speed);
-        }
+			this.y = (top + speed);
+		}
 
-        // move right
-        if (target.x > playerMiddleX + this._grace) {
-            var distance = Math.abs(target.x - playerMiddleX);
-            let speed = this.calculateSpeed(distance, doubleSpeed);
+		// move right
+		if (target.x > playerMiddleX + this._grace) {
+			var distance = Math.abs(target.x - playerMiddleX);
+			let speed = this.calculateSpeed(distance, doubleSpeed);
 
-            this.x = (left + speed);
-        }
-    }
+			this.x = (left + speed);
+		}
+	}
 
-    private calculateSpeed(distance: number, doubleSpeed: boolean = false): number {
-        var speed = distance / (doubleSpeed ? this._lag * 0.5 : this._lag);
-        speed = speed < 4 ? 4 : speed;
-        return speed;
-    }
+	private calculateSpeed(distance: number, doubleSpeed: boolean = false): number {
+		var speed = distance / (doubleSpeed ? this._lag * 0.5 : this._lag);
+		speed = speed < 4 ? 4 : speed;
+		return speed;
+	}
 }
+
