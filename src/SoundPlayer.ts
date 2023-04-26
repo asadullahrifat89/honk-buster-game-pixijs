1e977a43a1c6c8b41fb726c033255f67a2e31186
Asadullah Rifat <asadullah.rifat>
2023-04-26 15:06:07 +0600

    sound object and player added

---
+++ src/SoundPlayer.ts
@@ -0,0 +1,61 @@
+﻿import { Constants, SoundType } from './Constants';
+import { SoundObject } from './SoundObject';
+
+
+export class SoundPlayer {
+
+    private audioObjects: SoundObject[];
+
+    constructor(soundType: SoundType, volume: number, loop: boolean) {
+
+        // hold all sound objects for the sound type
+        this.audioObjects = Constants.SOUND_TEMPLATES.filter(x => x.soundType == soundType).map(u => new SoundObject(u.uri, volume, loop));
+    }
+
+    play() {
+        this.audioObjects.forEach(audioObject => {
+            if (audioObject.isPlaying())
+                audioObject.stop();
+        });
+
+        var index = Constants.getRandomNumber(0, this.audioObjects.length - 1);
+        var audioObject = this.audioObjects[index];
+
+        if (audioObject)
+            audioObject.play();
+    }
+
+    pause() {
+        var playings = this.audioObjects.filter(x => x.isPlaying());
+
+        if (playings) {
+            playings.forEach(playing => {
+                playing.pause();
+            });
+        }
+    }
+
+    resume() {
+        var playings = this.audioObjects.filter(x => x.isPaused());
+
+        if (playings) {
+            playings.forEach(playing => {
+                playing.resume();
+            });
+        }
+    }
+
+    stop() {
+        this.audioObjects.forEach(audioObject => {
+            if (audioObject.isPlaying())
+                audioObject.stop();
+        });
+    }
+
+    setVolume(volume: number) {
+        this.audioObjects.forEach(audioObject => {
+            if (audioObject.isPlaying())
+                audioObject.setVolume(volume);
+        });
+    }
+}
