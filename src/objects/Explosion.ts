880607d5b01707380c2fae417eee947f533226c3
Asadullah Rifat <anonymusrif@gmail.com>
2023-05-03 00:10:48 +0600

    refact animated sprite

--- src/objects/Explosion.ts
+++ src/objects/Explosion.ts
@@ -31,27 +31,30 @@ export class Explosion extends GameObjectContainer {
 
 function getExplosionAnimation(): AnimatedSprite {
 
+	const spriteWidth: number = 138;
+	const spriteHeight: number = 138;
+
 	// explosion 1
 	const atlasData: SpriteSheetJson = {
 		frames: {
 			frame0: {
-				frame: { x: 138 * 0, y: 0, w: 138, h: 138 },
-				sourceSize: { w: 138, h: 138 },
+				frame: { x: spriteWidth * 0, y: 0, w: spriteWidth, h: spriteHeight },
+				sourceSize: { w: spriteWidth, h: spriteHeight },
 				spriteSourceSize: { x: 0, y: 0 }
 			},
 			frame1: {
-				frame: { x: 138 * 1, y: 0, w: 138, h: 138 },
-				sourceSize: { w: 138, h: 138 },
+				frame: { x: spriteWidth * 1, y: 0, w: spriteWidth, h: spriteHeight },
+				sourceSize: { w: spriteWidth, h: spriteHeight },
 				spriteSourceSize: { x: 0, y: 0 }
 			},
 			frame2: {
-				frame: { x: 138 * 2, y: 0, w: 138, h: 138 },
-				sourceSize: { w: 138, h: 138 },
+				frame: { x: spriteWidth * 2, y: 0, w: spriteWidth, h: spriteHeight },
+				sourceSize: { w: spriteWidth, h: spriteHeight },
 				spriteSourceSize: { x: 0, y: 0 }
 			},
 			frame3: {
-				frame: { x: 138 * 3, y: 0, w: 138, h: 138 },
-				sourceSize: { w: 138, h: 138 },
+				frame: { x: spriteWidth * 3, y: 0, w: spriteWidth, h: spriteHeight },
+				sourceSize: { w: spriteWidth, h: spriteHeight },
 				spriteSourceSize: { x: 0, y: 0 }
 			},
 		},
@@ -63,72 +66,6 @@ function getExplosionAnimation(): AnimatedSprite {
 			frames: ["frame0", "frame1", "frame2", "frame3"]
 		}
 	};
-
-	// explosion 2
-	//const atlasData: SpriteSheetJson = {
-	//	frames: {
-	//		frame0: {
-	//			frame: { x: 283.33 * 0, y: 237.5 * 0, w: 283.33, h: 237.5 },
-	//			sourceSize: { w: 283.33, h: 237.5 },
-	//			spriteSourceSize: { x: 0, y: 0 }
-	//		},
-	//		frame1: {
-	//			frame: { x: 283.33 * 1, y: 237.5 * 0, w: 283.33, h: 237.5 },
-	//			sourceSize: { w: 283.33, h: 237.5 },
-	//			spriteSourceSize: { x: 0, y: 0 }
-	//		},
-	//		frame2: {
-	//			frame: { x: 283.33 * 2, y: 237.5 * 0, w: 283.33, h: 237.5 },
-	//			sourceSize: { w: 283.33, h: 237.5 },
-	//			spriteSourceSize: { x: 0, y: 0 }
-	//		},	
-
-	//		//frame3: {
-	//		//	frame: { x: 283.33 * 0, y: 237.5 * 1, w: 283.33, h: 237.5 },
-	//		//	sourceSize: { w: 283.33, h: 237.5 },
-	//		//	spriteSourceSize: { x: 0, y: 0 }
-	//		//},
-	//		//frame4: {
-	//		//	frame: { x: 283.33 * 1, y: 237.5 * 1, w: 283.33, h: 237.5 },
-	//		//	sourceSize: { w: 283.33, h: 237.5 },
-	//		//	spriteSourceSize: { x: 0, y: 0 }
-	//		//},
-	//		//frame5: {
-	//		//	frame: { x: 283.33 * 2, y: 237.5 * 1, w: 283.33, h: 237.5 },
-	//		//	sourceSize: { w: 283.33, h: 237.5 },
-	//		//	spriteSourceSize: { x: 0, y: 0 }
-	//		//},
-
-	//		//frame6: {
-	//		//	frame: { x: 283.33 * 0, y: 237.5 * 2, w: 283.33, h: 237.5 },
-	//		//	sourceSize: { w: 283.33, h: 237.5 },
-	//		//	spriteSourceSize: { x: 0, y: 0 }
-	//		//},
-	//		//frame7: {
-	//		//	frame: { x: 283.33 * 1, y: 237.5 * 2, w: 283.33, h: 237.5 },
-	//		//	sourceSize: { w: 283.33, h: 237.5 },
-	//		//	spriteSourceSize: { x: 0, y: 0 }
-	//		//},
-	//		//frame8: {
-	//		//	frame: { x: 283.33 * 2, y: 237.5 * 2, w: 283.33, h: 237.5 },
-	//		//	sourceSize: { w: 283.33, h: 237.5 },
-	//		//	spriteSourceSize: { x: 0, y: 0 }
-	//		//},
-
-	//		//frame9: {
-	//		//	frame: { x: 283.33 * 1, y: 237.5 * 3, w: 283.33, h: 237.5 },
-	//		//	sourceSize: { w: 283.33, h: 237.5 },
-	//		//	spriteSourceSize: { x: 0, y: 0 }
-	//		//},
-	//	},
-	//	meta: {
-	//		image: './images/explosion_2.png',
-	//		scale: "1",
-	//	},
-	//	animations: {
-	//		frames: ["frame0", "frame1", "frame2", /*"frame3", "frame4", "frame5", "frame6", "frame7", "frame8", "frame9"*/]
-	//	}
-	//};
 
 	// Create the SpriteSheet from data and image
 	const spritesheet: Spritesheet = new Spritesheet(BaseTexture.from(atlasData.meta.image), atlasData);
