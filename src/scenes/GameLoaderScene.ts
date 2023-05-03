bc74657e79f5f57888b194edde07f726735d0216
Asadullah Rifat <anonymusrif@gmail.com>
2023-05-03 11:17:32 +0600

    game loader scene fixed

--- src/scenes/GameLoaderScene.ts
+++ src/scenes/GameLoaderScene.ts
@@ -1,4 +1,4 @@
-import { Container, Graphics, Assets, Text } from "pixi.js";
+import { Container, Graphics, Assets } from "pixi.js";
 import { manifest } from "../assets";
 import { IScene } from "../managers/IScene";
 import { SceneManager } from "../managers/SceneManager";
@@ -11,28 +11,20 @@ export class GameLoaderScene extends Container implements IScene {
 	// for making our loader graphics...
 	private loaderBar: Container;
 	private loaderBarBoder: Graphics;
-	private loaderBarFill: Graphics;
-	private changeOrienationText: Text;
+	private loaderBarFill: Graphics;	
 
 	constructor() {
-		super();
-
-		//this.filters = [new DropShadowFilter()];
+		super();		
 
 		// lets make a loader graphic:
 		const loaderBarWidth = SceneManager.width * 0.8; // just an auxiliar variable
 
 		// the fill of the bar.
-		this.loaderBarFill = new Graphics();
-		this.loaderBarFill.beginFill(0x5fc4f8, 1);
-		this.loaderBarFill.drawRoundedRect(0, 0, loaderBarWidth, 50, 5);
-		this.loaderBarFill.endFill();
+		this.loaderBarFill = new Graphics().beginFill(0x5fc4f8, 1).drawRoundedRect(0, 0, loaderBarWidth, 50, 5).endFill();		
 		this.loaderBarFill.scale.x = 0; // we draw the filled bar and with scale we set the %
 
 		// The border of the bar.
-		this.loaderBarBoder = new Graphics();
-		this.loaderBarBoder.lineStyle(10, 0xffffff, 1);
-		this.loaderBarBoder.drawRoundedRect(0, 0, loaderBarWidth, 50, 5);
+		this.loaderBarBoder = new Graphics().lineStyle(5, 0xffffff, 1).drawRoundedRect(0, 0, loaderBarWidth, 50, 5);		
 
 		// Now we keep the border and the fill in a container so we can move them together.
 		this.loaderBar = new Container();
@@ -42,23 +34,10 @@ export class GameLoaderScene extends Container implements IScene {
 		//Looks complex but this just centers the bar on screen.
 		this.loaderBar.position.x = (SceneManager.width - this.loaderBar.width) / 2;
 		this.loaderBar.position.y = (SceneManager.height - this.loaderBar.height) / 2;
-		this.addChild(this.loaderBar);
-
-		this.changeOrienationText = new Text("Pls Change Screen Orientation", {
-			fontFamily: "gamefont",
-			fontSize: 18,
-			align: "center",
-			fill: "#ffffff",
-		});
-		this.changeOrienationText.x = SceneManager.width / 2 - this.changeOrienationText.width / 2;
-		this.changeOrienationText.y = (SceneManager.height / 2 - this.changeOrienationText.height / 2) - 120;
-		this.changeOrienationText.alpha = 0;
-
-		this.addChild(this.changeOrienationText);
+		this.addChild(this.loaderBar);		
 
 		// Start loading!
-		this.initializeLoader().then(() => {
-			// Remember that constructors can't be async, so we are forced to use .then(...) here!
+		this.initializeLoader().then(() => {			
 			this.gameLoaded();
 		})
 	}
@@ -88,19 +67,12 @@ export class GameLoaderScene extends Container implements IScene {
 		// Our game finished loading!
 
 		// check if screen orientation is in correct mode
-		if (SceneManager.width < SceneManager.height) {
-
-			// Let's remove our loading bar
-			this.removeChild(this.loaderBar);
-
-			// Change scene to the menu scene!
+		if (SceneManager.width < SceneManager.height) {			
+			this.removeChild(this.loaderBar);			
 			SceneManager.changeScene(new ScreenOrientationScene());
 		}
-		else {
-			// Let's remove our loading bar
-			this.removeChild(this.loaderBar);
-
-			// Change scene to the menu scene!
+		else {			
+			this.removeChild(this.loaderBar);			
 			SceneManager.changeScene(new GameTitleScene());
 		}
 	}
