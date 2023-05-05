d1329678ad9187d6d8b7cd279b4919da8cbfe893
Asadullah Rifat <anonymusrif@gmail.com>
2023-05-05 17:35:59 +0600

    how to play page developing

--- src/scenes/GameInstructionsScene.ts
+++ src/scenes/GameInstructionsScene.ts
@@ -48,7 +48,8 @@ export class GameInstructionsScene extends Container implements IScene {
 		title.y = (this.uiContainer.height / 2 - title.height / 2) - 120;
 		this.uiContainer.addChild(title);
 
-		const joystick = new Container();
+		// joy stick
+		const joystick = new GameObjectContainer();
 
 		const joystick_sprite: GameObjectSprite = new GameObjectSprite(Texture.from("joystick"));
 		joystick_sprite.width = 256 / 2;
@@ -65,16 +66,17 @@ export class GameInstructionsScene extends Container implements IScene {
 		const joystick_controller_container = new GameObjectContainer();
 		joystick_controller_container.addChild(joystick_sprite);
 		joystick_controller_container.addChild(joystick_handle_sprite);
-		joystick_controller_container.setPosition(this.uiContainer.width / 2 - joystick_sprite.width * 3, this.uiContainer.height / 2 - joystick_sprite.height / 2 + 10);
 		joystick.addChild(joystick_controller_container);
 
-		const joystick_msg = new MessageBubble(0, "Use the 🕹️ or ⌨️ arrow keys to move.", 20);
+		const joystick_msg = new MessageBubble(0, "Use this or ⌨️ arrow keys to move.", 20);
 		joystick_msg.setPosition(joystick_controller_container.x + joystick_controller_container.width, joystick_controller_container.y + 50);
 		joystick.addChild(joystick_msg);
 
+		joystick.setPosition(this.uiContainer.width / 2 - joystick.width / 2, (this.uiContainer.height / 2 - joystick.height / 2) + 10);
 		this.uiContainer.addChild(joystick);
 
-		const attack_button = new Container();
+		// attack button
+		const attack_button = new GameObjectContainer();
 		attack_button.renderable = false;
 
 		const attack_button_sprite: GameObjectSprite = new GameObjectSprite(Texture.from("attack_button"));
@@ -85,14 +87,36 @@ export class GameInstructionsScene extends Container implements IScene {
 
 		const attack_button_container = new GameObjectContainer();
 		attack_button_container.addChild(attack_button_sprite);
-		attack_button_container.setPosition(this.uiContainer.width / 2 - attack_button_sprite.width * 4, this.uiContainer.height / 2 - attack_button_sprite.height / 2 + 10);
 		attack_button.addChild(attack_button_container);
 
 		const attack_button_msg = new MessageBubble(0, "Press this or ⌨️ space key to attack.", 20);
 		attack_button_msg.setPosition(attack_button_container.x + attack_button_container.width + 10, attack_button_container.y + 25);
 		attack_button.addChild(attack_button_msg);
 
+		attack_button.setPosition(this.uiContainer.width / 2 - attack_button.width / 2, (this.uiContainer.height / 2 - attack_button.height / 2) + 10);
 		this.uiContainer.addChild(attack_button);
+
+		// cars
+
+		const car = new GameObjectContainer();
+		car.renderable = false;
+
+		const car_sprite: GameObjectSprite = new GameObjectSprite(Texture.from("vehicle_small_1"));
+		car_sprite.width = 256 / 2;
+		car_sprite.height = 256 / 2;
+		car_sprite.x = 0;
+		car_sprite.y = 0;
+
+		const car_container = new GameObjectContainer();
+		car_container.addChild(car_sprite);		
+		car.addChild(car_container);
+
+		const car_msg = new MessageBubble(0, "Drop bombs on honking cars.", 20);
+		car_msg.setPosition(car_container.x + car_container.width + 10, car_container.y + 25);
+		car.addChild(car_msg);
+
+		car.setPosition(this.uiContainer.width / 2 - car.width / 2, (this.uiContainer.height / 2 - car.height / 2) + 10);
+		this.uiContainer.addChild(car);
 
 		const button = new Button(() => {
 
@@ -104,6 +128,10 @@ export class GameInstructionsScene extends Container implements IScene {
 			}
 			else if (attack_button.renderable) {
 				attack_button.renderable = false;
+				car.renderable = true;
+			}
+			else if (car.renderable) {
+
 			}
 			else {
 				this.removeChild(this.uiContainer);
