eccef32af4f2e3649ea1a974918886646ea3dfac
Asadullah Rifat <anonymusrif@gmail.com>
2023-05-12 21:03:33 +0600

    fixing sound playing bug

--- src/core/SoundObjectPlayer.ts
+++ src/core/SoundObjectPlayer.ts
@@ -18,7 +18,7 @@ export class SoundObjectPlayer {
 
 	play(): number {
 		this.soundObjects.forEach(audioObject => {
-			if (audioObject.isPlaying() || audioObject.isLooping())
+			if (audioObject.isPlaying() && audioObject.isLooping()) // stop a looping sound
 				audioObject.stop();
 		});
 
